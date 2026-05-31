e97133082786b13ff475a47f12d1e09285f9fff5
bhuvanesh waran <bhuvaneshwaranoffl@gmail.com>
2026-05-31 17:04:08 +0530

    animation added.

--- lib/core/theme/app_text_styles.dart
+++ lib/core/theme/app_text_styles.dart
@@ -8,8 +8,8 @@ abstract final class AppTextStyles {
 
   static const brandLabel = TextStyle(
     fontFamily: _fontFamily,
-    fontWeight: FontWeight.w500,
-    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
     color: AppColors.textPrimary,
   );
 
