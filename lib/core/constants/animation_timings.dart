dbeb2f3004f7bb617f68ccbbf01748888976da0b
bhuvanesh waran <bhuvaneshwaranoffl@gmail.com>
2026-05-31 19:10:48 +0530

    animation timing modified

--- lib/core/constants/animation_timings.dart
+++ lib/core/constants/animation_timings.dart
@@ -5,11 +5,19 @@ abstract final class AnimationTimings {
 
   static const walletImageFadeIn = Interval(0.0, 0.15, curve: Curves.easeOut);
   static const brandTextFadeIn = Interval(0.15, 0.30, curve: Curves.easeOut);
-  static const headerSlideUp = Interval(0.35, 0.55, curve: Curves.easeInOutCubic);
-  
+  static const headerSlideUp = Interval(
+    0.35,
+    0.55,
+    curve: Curves.easeInOutCubic,
+  );
+
   static const featureListFadeInStart = 0.50;
   static const featureListFadeInEnd = 0.75;
 
   static const footerFadeIn = Interval(0.75, 0.90, curve: Curves.easeOut);
-  static const headerButtonsFadeIn = Interval(0.75, 0.90, curve: Curves.easeOut);
+  static const headerButtonsFadeIn = Interval(
+    0.75,
+    0.90,
+    curve: Curves.easeOut,
+  );
 }
